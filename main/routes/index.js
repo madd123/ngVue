70ce0fe720bbc85a5e6e7305fe17dfb9d458162f
WPH2017 <2974769672@qq.com>
2017-09-30 15:41:51 +0800

    0930

--- main/routes/index.js
+++ main/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/shop', function(req, res, next) {
+    res.render('shop/index', { title: 'Express' });
+});
+
+router.get('/docs', function(req, res, next) {
+    res.render('docs/index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login/index', { title: 'Express' });
+});
+
 module.exports = router;
