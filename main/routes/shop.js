bb70d4951752eb6a7dc9d06154ad465846068c30
WPH2017 <2974769672@qq.com>
2017-10-06 18:02:53 +0800

    1006
    ~~商城初步样式

--- main/routes/shop.js
+++ main/routes/shop.js
@@ -5,7 +5,7 @@ router.get('/',function (req,res,next) {
    res.render('shop/index',{});
 });
 router.get('/settle',function (req,res,next) {
-   res.render('shop/settle',{title:'这是结算页面'});
+   res.render('shop/settle',{title:'The Vue Shop'});
 });
 
 module.exports = router;
