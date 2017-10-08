65597cc0c9fe7c23fd572a0f793a9f2b92b1a5a0
Bibiboomer <1321160783@qq.com>
2017-10-08 19:35:29 +0800

    1934
    text

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,13 +1,29 @@
+
 var express = require('express');
 var router = express.Router();
+
+var Person ={
+    title:"Erpes是的0违法未到法定",
+
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/lm', function(req, res, next) {
+    res.render('lm/index', Person
+);
+  });
+
+
+  router.get('/lm/123', function(req, res, next) {
+    res.render('lm/index', { title: 'Expr第二个ess' });
+  });
+
 router.get('/shop', function(req, res, next) {
-    res.render('shop/index', { title: 'Express' });
+    res.render('shop/index', { title: '测试' });
 });
 
 router.get('/docs', function(req, res, next) {
@@ -31,3 +47,4 @@ router.get('/register', function(req, res, next) {
 });
 
 module.exports = router;
+
